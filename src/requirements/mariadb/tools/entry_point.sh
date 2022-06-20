1a285dcb6ea786e449f5141252c22b6ce6b41d81
Tamirat Lemma <tamworku1@gmail.com>
2022-06-20 11:29:15 +0200

    env added

--- src/requirements/mariadb/tools/entry_point.sh
+++ src/requirements/mariadb/tools/entry_point.sh
@@ -1,12 +1,22 @@
 chown -R mysql:mysql /var/lib/mysql
 
+cat<<EOF > init.sql
+USE mysql;
+FLUSH PRIVILEGES ;
+
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$DB_ROOT_PASS');
+
+CREATE DATABASE IF NOT EXISTS wp CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER IF NOT EXISTS 'owner'@'%' IDENTIFIED BY '$DB_ADMIN_PASS';
+-- CREATE USER IF NOT EXISTS 'dev'@'%' IDENTIFIED BY 'devpass';
+
+GRANT ALL PRIVILEGES ON wp.* TO 'owner'@'%' IDENTIFIED BY '$DB_ADMIN_PASS' WITH GRANT OPTION; 
+-- GRANT ALL PRIVILEGES ON wp.* TO 'dev'@'%' IDENTIFIED BY 'devpass' WITH GRANT OPTION;
+
+FLUSH PRIVILEGES ;
+EOF
+
 /usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --verbose=0 --skip-networking=0 --bootstrap < /init.sql
 
 exec /usr/bin/mysqld --user=mysql --datadir=/var/lib/mysql --console
-# while !(mysqladmin ping -S /tmp/mysqld.sock)
-# do
-#    sleep 3
-#    echo "waiting for mysql ..."
-# done
-# echo "starting the inital script" 
-# cat init.sql | mariadb -S /tmp/mysqld.sock
+
--- src/requirements/wordpress/tools/setup.sh
+++ src/requirements/wordpress/tools/setup.sh
@@ -1,13 +1,22 @@
 #!/bin/sh
 
+# wait for mysql
+# while ! mariadb -h$MYSQL_HOST -u$WP_DATABASE_USR -p$WP_DATABASE_PWD $WP_DATABASE_NAME &>/dev/null; do
+#     sleep 3
+# done
+
 if [ ! -f "/home/www/wp-config.php" ]; then
-    wp core download --path=/home/www/
-    cd /home/www/
-    wp config create --dbname=$DB_NAME --dbuser=$DB_ADMIN \
-    --dbpass=$DB_ADMIN_PASS --dbhost=$DB_HOST
-    # wp db create
-    wp core install --url=$SITE_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN \
-    --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL 
+    # wp core download --path=/home/www/
+    wget http://wordpress.org/latest.tar.gz 
+    tar -xf latest.tar.gz 
+    mv wordpress/* /home/www/
+    # cd /home/www/
+    # wp config create --dbname=$DB_NAME --dbuser=$DB_ADMIN \
+    # --dbpass=$DB_ADMIN_PASS --dbhost=$DB_HOST
+    # # wp db create
+    # wp core install --url=$SITE_NAME --title=$SITE_TITLE --admin_user=$WP_ADMIN \
+    # --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+    # wp theme install inspiro --activate 
 fi
 
 
@@ -22,3 +31,6 @@ TIMEZONE="Europe/Helsinki"
 cp /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
 echo "${TIMEZONE}" > /etc/timezone
 sed -i "s|;*date.timezone =.*|date.timezone = ${TIMEZONE}|i" /etc/php8/php.ini
+
+#start php
+/usr/sbin/php-fpm8 -F -R
