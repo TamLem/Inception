336ffe8c25d0e3ae498ad2cc6c1a3c0f45c04ea0
Tamirat Lemma <tamworku1@gmail.com>
2022-06-21 13:05:48 +0200

    added ftp and redis

---
+++ src/requirements/bonus/ftp/tools/config.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+mkdir -p /var/www/html
+
+adduser $FTP_USER --disabled-password
+echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
+chown -R $FTP_USER:$FTP_USER /var/www/html
+
+echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
+
+echo "FTP started on port 20..."
+
+vsftpd /etc/vsftpd/vsftpd.conf
